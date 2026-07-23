37bcce0cabb16bbbcd37249760b840a864114921
sober213 <dnjs783@gmail.com>
2026-07-23 22:24:39 +0900

    [Passed] 배수의 개수 (50ms, 16MB)

---
+++ trail0/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,15 @@
+
+count_3 = 0
+count_5 = 0
+
+
+for _ in range(10):
+    number = int(input())
+    if number % 3 == 0:
+        count_3 += 1
+    if number % 5 == 0:
+            count_5 += 1
+print(count_3, count_5)        
+        
+
+    
