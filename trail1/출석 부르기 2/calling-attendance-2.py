b7a9f7a4a2c7a99796c1cc2c4f5ae79638acba10
sober213 <dnjs783@gmail.com>
2026-09-20 14:44:30 +0900

    [Passed] 출석 부르기 2 (46ms, 16MB)

---
+++ trail1/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    A = int(input())
+    if A == 1:
+        print('John')
+    elif A == 2:
+        print('Tom')
+    elif A == 3:
+        print('Paul')
+    elif A == 4:
+        print('Sam')
+    else:
+        print('Vacancy')
+        break
