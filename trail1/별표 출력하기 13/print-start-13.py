c6be5edbc01831ae69205b99faaba658cd49b6d3
sober213 <dnjs783@gmail.com>
2026-09-22 17:21:34 +0900

    [Passed] 별표 출력하기 13 (52ms, 16MB)

---
+++ trail1/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,19 @@
+N = int(input())
+for r in range(N):
+    if r % 2 == 1:
+        for c in range((r + 1) // 2):
+            print('*', end=' ')
+        print()
+    else:
+        for c in range(N - r // 2):
+            print('*', end=' ')
+        print()
+for r in range(N - 1, -1, -1):
+    if r % 2 == 1:
+        for c in range((r + 1) // 2):
+            print('*', end=' ')
+        print()
+    else:
+        for c in range(N - r // 2):
+            print('*', end=' ')
+        print()
