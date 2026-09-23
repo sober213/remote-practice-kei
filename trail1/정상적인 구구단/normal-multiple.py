1cca2cb9275a9837b099d2bf7b8d5c7f3b46f5af
sober213 <dnjs783@gmail.com>
2026-09-23 14:46:09 +0900

    [Passed] 정상적인 구구단 (51ms, 16MB)

---
+++ trail1/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,7 @@
+N = int(input())
+for r in range(1, N + 1):
+    for c in range(1, N + 1):
+        print(f'{r} * {c} = {r * c}', end='')
+        if r < N + 1 and c < N:
+            print(',', end=' ')
+    print()
