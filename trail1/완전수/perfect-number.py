9d5a9153631b8e13c15cb0922f7fefc3628eb0f0
sober213 <dnjs783@gmail.com>
2026-09-24 14:21:31 +0900

    [Passed] 완전수 (50ms, 16MB)

---
+++ trail1/완전수/perfect-number.py
@@ -0,0 +1,12 @@
+start, end = map(int, input().split())
+
+# Please write your code here.
+cnt = 0
+for i in range(start, end + 1):
+    total = 0
+    for j in range(1, i):
+        if i % j == 0:
+            total += j
+    if total == i:
+        cnt += 1
+print(cnt)
