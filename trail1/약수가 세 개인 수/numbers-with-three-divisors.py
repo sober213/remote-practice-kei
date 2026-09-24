ae5a6c4c54ae95fc93326ace94fd6fde53a52645
sober213 <dnjs783@gmail.com>
2026-09-24 14:23:25 +0900

    [Passed] 약수가 세 개인 수 (51ms, 17MB)

---
+++ trail1/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,12 @@
+start, end = map(int, input().split())
+
+# Please write your code here.
+t_cnt = 0
+for i in range(start, end + 1):
+    cnt = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 3:
+        t_cnt += 1
+print(t_cnt)
